97d27c796037ef37bb26a92e797d4c9afb5b86e6
Harry Qiu <hqiu678@outlook.com>
2022-01-14 14:12:53 +1100

    introduced cutting off channels for smaller filterbanks for heimdall.

--- fits2fil.py
+++ fits2fil.py
@@ -19,6 +19,9 @@ def _main():
     parser.add_argument('-o', '--output',type=str, default="test.fil",help='Output File Name')
     parser.add_argument('--ra',type=float, default=51436.202,help='RA')
     parser.add_argument('--dec',type=float, default=270330.24,help='Dec')
+    parser.add_argument('--ch1',type=int, default=0,help='starting channel')
+    parser.add_argument('--nchans',type=int, default=8192,help='total channels')
+
     parser.add_argument(dest='files', nargs='+')
     parser.set_defaults(verbose=False)
     values = parser.parse_args()
@@ -31,9 +34,9 @@ def _main():
             print()
             newdata=make_sigproc_object(rawdatafile  = filname,
                                         source_name = "bar",
-                                        nchans  = fits.nchan,
+                                        nchans  = values.nchans,
                                         foff = fits.foff, #MHz
-                                        fch1 = fits.fch1, # MHz
+                                        fch1 = fits.fch1+fits.foff*values.ch1, # MHz
                                         tsamp = hdu['SUBINT'].header["TBIN"], # seconds
                                         tstart = fits.tstart, #MJD
                                         src_raj=values.ra, # HHMMSS.SS
@@ -50,8 +53,10 @@ def _main():
                                         za_start=-1,
                                         )
             newdata.write_header(filname)
+
         print(filename)
         totaldata=fits.get_data(nstart=0,nsamp=1024*int(fits.nsubints))
+        writedata=totaldata[:,values.ch1:values.ch1+values.nchans]
         ### reads out stokes I data
         ### this step reads all subints to merge into one datachunk, can't stop printing subint readouts
 
